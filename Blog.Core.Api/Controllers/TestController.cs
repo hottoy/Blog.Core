46fbd68a652b4fbdfc746260855f8d8ea973920a
hottoy <hottoy@163.com>
2020-09-11 00:39:52 +0800

    Create TestController.cs
    修改端口8081 改成 8089 同时测试JWD登录

---
+++ Blog.Core.Api/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Blog.Core.Common.HttpContextUser;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Blog.Core.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IUser _user;
+        public TestController(IUser user)
+        {
+            this._user = user;
+        }
+        // GET: api/<TestController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TestController>/5
+        [HttpGet("{id}")]
+        [Authorize]
+        public string Get(int id)
+        {
+            return "你的登录名："+_user.RealName+id.ToString();
+        }
+
+        // POST api/<TestController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TestController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TestController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
